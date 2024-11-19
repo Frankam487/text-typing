4c927dbcc2b324c0731bc9b4e1b692a91b573edf
Frankam487 <fkamgang06@gmail.com>
2024-11-19 10:53:42 -0800

    initialization

--- app.jsx
+++ app.jsx
@@ -1,1 +1,7 @@
-alert('bb')
+const target = document.getElementById("target");
+const letter = () => {
+
+    let array = ["developpeur", "Photographe", "Creatif"];
+    target.textContent = array[0];
+}
+letter();
